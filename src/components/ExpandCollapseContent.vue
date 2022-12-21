3a994f4e7087f5b65c0ac00add3450dd5d7ed25b
DESKTOP-DCLTBK8\rothw <rothwell.andy@gmail.com>
2022-12-21 09:53:44 -0500

    first commit

---
+++ src/components/ExpandCollapseContent.vue
@@ -0,0 +1,395 @@
+<template>
+  <div class="main-content">
+    <div>
+      Date: {{ item.fields.Date }}<br>
+      Type: {{ item.fields.type }}
+    </div>
+    <div
+      v-if="item.fields.Attachments"
+      class="img-div"
+    >
+      <img :src="item.fields.Attachments[0].url">
+    </div>
+    <div
+      v-if="!item.fields.Attachments"
+    >
+      No photo
+    </div>
+    <!-- <div>
+      {{ item.fields.Name }}
+    </div> -->
+    <!-- <div class="columns">
+      <div class="column">
+        <div
+          v-if="item.attributes.ADDRESS"
+          class="columns is-mobile"
+        >
+          <div class="column is-1">
+            <font-awesome-icon icon="map-marker-alt" />
+          </div>
+          <div class="column">
+            {{ item.attributes.ADDRESS }}<br>
+            Philadelphia, PA {{ item.attributes.ZIP_CODE }}<br>
+          </div>
+        </div>
+
+        <div
+          v-if="item.attributes.WEBSITE"
+          class="columns is-mobile"
+        >
+          <div class="column is-1">
+            <font-awesome-icon icon="globe" />
+          </div>
+          <div class="column">
+            <a
+              target="_blank"
+              :href="item.attributes.WEBSITE"
+            >{{ $t('website') }}</a>
+          </div>
+        </div>
+        <div
+          v-if="item.attributes.OFFICE_PHONE_NUM"
+          class="columns is-mobile"
+        >
+          <div class="column is-1">
+            <font-awesome-icon icon="phone" />
+          </div>
+          <div class="column">
+            {{ item.attributes.OFFICE_PHONE_NUM }}
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="columns is-mobile">
+          <div
+            class="column is-1"
+          >
+            <font-awesome-icon icon="car-bus" />
+          </div>
+          <div class="column">
+            <div>{{ item.attributes.PARKING_TRANSIT }}</div>
+
+          </div>
+        </div>
+
+      </div>
+    </div> -->
+
+    <!-- <h3>{{ $t('ageSpecificServices') }}</h3>
+
+    <vue-good-table
+      :columns="ageSpecificServices.columns"
+      :rows="ageSpecificServices.rows"
+      :sort-options="{ enabled: false }"
+      styleClass="vgt-table condensed"
+    >
+      <template slot="table-column" slot-scope="props">
+        <span
+          v-if="props.column.label =='Service'"
+          class="table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </span>
+        <div
+          v-if="props.column.label =='Adult'"
+          class="center table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </div>
+        <div
+          v-if="props.column.label =='Child'"
+          class="center table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </div>
+        <div
+          v-if="props.column.label =='Existing'"
+          class="center table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </div>
+      </template>
+
+      <template slot="table-row" slot-scope="props">
+        <span
+          v-if="props.column.field == 'service'"
+          class="table-text"
+        >
+          {{ $t(props.row.service) }}
+        </span>
+        <div
+          v-if="props.column.field == 'adult'"
+          class="center"
+        >
+          <font-awesome-icon
+            v-if="['Yes', 'Established Patients'].includes(props.row.adult)"
+            :icon="['fa', 'check']"
+          />
+        </div>
+        <div
+          v-if="props.column.field == 'child'"
+          class="center"
+        >
+          <font-awesome-icon
+            v-if="['Yes', 'Established Patients'].includes(props.row.child)"
+            :icon="['fa', 'check']"
+          />
+        </div>
+        <div
+          v-if="props.column.field == 'existing'"
+          class="center"
+        >
+          <font-awesome-icon
+            v-if="props.row.existing.includes('Established Patients')"
+            :icon="['fa', 'check']"
+          />
+        </div>
+      </template>
+    </vue-good-table>
+
+    <h3 class="section-heading">{{ $t('otherServices') }}</h3>
+    <vue-good-table
+      :columns="otherServices.columns"
+      :rows="otherServices.rows"
+      :sort-options="{ enabled: false }"
+      styleClass="vgt-table condensed"
+    >
+      <template slot="table-column" slot-scope="props">
+        <span
+          v-if="props.column.label =='Service'"
+          class="table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </span>
+        <div
+          v-if="props.column.label =='All'"
+          class="center table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </div>
+        <div
+          v-if="props.column.label =='Existing'"
+          class="center table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </div>
+      </template>
+
+      <template slot="table-row" slot-scope="props">
+        <span
+          v-if="props.column.field == 'service'"
+          class="table-text"
+        >
+          {{ $t(props.row.service) }}
+        </span>
+        <div
+          v-if="props.column.field == 'all'"
+          class="center"
+        >
+          <font-awesome-icon
+            v-if="props.row.value == 'Yes'"
+            :icon="['fa', 'check']"
+          />
+        </div>
+        <div
+          v-if="props.column.field == 'existing'"
+          class="center"
+        >
+          <font-awesome-icon
+            v-if="['Yes', 'Established Patients'].includes(props.row.value)"
+            :icon="['fa', 'check']"
+          />
+        </div>
+      </template>
+    </vue-good-table>
+
+    <h3 class="section-heading">{{ $t('hours') }}</h3>
+    <vue-good-table
+      :columns="hours.columns"
+      :rows="hours.rows"
+      :sort-options="{ enabled: false }"
+      styleClass="vgt-table condensed"
+    >
+      <template slot="table-column" slot-scope="props">
+        <span
+          v-if="props.column.label =='Days'"
+          class="table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </span>
+        <span
+          v-if="props.column.label =='Schedule'"
+          class="table-header-text"
+        >
+          {{ $t(props.column.i18nLabel) }}
+        </span>
+      </template>
+
+      <template slot="table-row" slot-scope="props">
+        <span
+          v-if="props.column.field == 'days'"
+          class="table-text"
+        >
+          {{ $t(props.row.days) }}
+        </span>
+        <div
+          v-if="props.column.field == 'schedule'"
+          class="table-text"
+        >
+          {{ props.row.schedule }}
+        </div>
+      </template>
+    </vue-good-table>
+
+    <h3 class="section-heading">{{ $t( 'tests.category' ) }}</h3>
+    <span
+      v-for="(type, index) of tests"
+      :key="'test-'+index"
+    >
+      {{ $t('tests[\'' + type.toLowerCase() + '\']') }}<span v-if="index<tests.length-1">, </span>
+    </span>
+
+    <h3 class="section-heading">{{ $t( 'slidingScale' ) }}</h3>
+    <div>
+    </div>
+
+    <h3 class="section-heading">{{ $t( 'languages.languagesSpoken' ) }}</h3>
+    <span
+      v-for="(language, index) of languagesSpoken"
+      :key="'lang-'+index"
+    >
+      {{ $t('languages[\'' + language.toLowerCase() + '\']') }}<span v-if="index<languagesSpoken.length-1">, </span>
+    </span> -->
+
+  </div>
+</template>
+
+<script>
+
+import SharedFunctions from './mixins/SharedFunctions.vue';
+import { VueGoodTable } from 'vue-good-table';
+import 'vue-good-table/dist/vue-good-table.css'
+
+export default {
+  name: 'ExpandCollapseContent',
+  components: {
+    VueGoodTable,
+  },
+  mixins: [ SharedFunctions ],
+  props: {
+    item: {
+      type: Object,
+      default: function(){
+        return {};
+      },
+    },
+  },
+  computed: {
+
+  },
+  // methods: {
+  //   parseAddress(address) {
+  //     const formattedAddress = address.replace(/(Phila.+)/g, city => `<div>${city}</div>`).replace(/^\d+\s[A-z]+\s[A-z]+/g, lineOne => `<div>${lineOne}</div>`).replace(/,/, '');
+  //     return formattedAddress;
+  //   },
+  // },
+};
+
+</script>
+
+<style lang="scss">
+
+table {
+  border: 0px none;
+  border-style: none !important;
+}
+
+.img-div {
+  padding-left: 100px;
+  padding-right: 100px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+// .vgt-table.bordered td, .vgt-table.bordered th {
+//   border: 0px none #dcdfe6;
+// }
+
+// th, td {
+//   padding-top: 7px !important;
+//   padding-bottom: 7px !important;
+// }
+
+.vgt-responsive {
+  // overflow-x: hidden;
+}
+
+th {
+  background: #ffffff !important;
+}
+
+tr:nth-child(odd) {
+  background: #eee;
+}
+
+.center {
+  text-align: center;
+}
+
+.table-text {
+  font-family: "OpenSans-Regular", "Open Sans", sans-serif;
+  font-size: 14px;
+}
+
+.table-header-text {
+  font-family: "OpenSans-SemiBold", "Open Sans SemiBold", "Open Sans", sans-serif;
+  font-size: 14px;
+}
+
+h3 {
+  font-family: "Montserrat-SemiBold", "Montserrat SemiBold", "Montserrat", sans-serif;
+  font-weight: 650;
+  font-size: 24px;
+  padding-top: 14px;
+  padding-bottom: 14px;
+}
+
+.section-heading {
+  margin-top: 14px;
+}
+
+// .table-title {
+//   font-family: "Montserrat-SemiBold", "Montserrat SemiBold", "Montserrat", sans-serif;
+//   font-weight: 650;
+//   font-size: 24px;
+//   padding-top: 14px;
+//   padding-bottom: 14px;
+// }
+
+.no-wrap {
+  white-space: nowrap;
+}
+
+.no-margins {
+  margin: 0px;
+  margin-bottom: 0px !important;
+}
+
+.top-margins {
+  margin-top: 10px;
+}
+
+.main-content {
+  font-size: 14px,
+}
+
+.td-style {
+  font-size: 14px !important;
+}
+
+.td-textbox {
+  padding-left: 2rem;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,289 @@
+// this is the base-config for resource-finder
+// the point of this file is that it will move outside the project
+// (so that settings we put in it can be used by other projects)
+// and be pulled in with an axios call or something
+// (we might not need to use axios with new vue async tools)
+// if that is not needed, we can move this info to main.js
+
+
+// turn off console logging in production
+if (process.env.NODE_ENV === 'production') {
+  console.log = console.info = console.debug = console.error = function () {};
+}
+console.log('main.js process.env.NODE_ENV:', process.env.NODE_ENV, 'process.env.VUE_APP_PUBLICPATH:', process.env.VUE_APP_PUBLICPATH);
+
+// Font Awesome Icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons/faExclamationTriangle';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons/faCalendarAlt';
+import { faBuilding } from '@fortawesome/free-solid-svg-icons/faBuilding';
+import { faUserMd } from '@fortawesome/free-solid-svg-icons/faUserMd';
+import { faCircle } from '@fortawesome/free-solid-svg-icons/faCircle';
+import { faCar } from '@fortawesome/free-solid-svg-icons/faCar';
+import { faCheckSquare } from '@fortawesome/free-regular-svg-icons/faCheckSquare';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+import { faSquare } from '@fortawesome/free-regular-svg-icons/faSquare';
+library.add(faExclamationTriangle, faCalendarAlt, faBuilding, faUserMd, faCircle, faCar, faCheckSquare, faSquare, faCheck);
+
+// import pinboard
+import pinboard from '@phila/pinboard/src/main.js';
+
+// data-sources
+import hikes from './data-sources/hikes';
+
+import expandCollapseContent from './components/ExpandCollapseContent.vue';
+// import customGreeting from './components/customGreeting.vue';
+const customComps = {
+  'expandCollapseContent': expandCollapseContent,
+  // 'customGreeting': customGreeting,
+};
+
+import '@creativebulma/bulma-tooltip/dist/bulma-tooltip.min.css'
+
+pinboard({
+  alerts: {
+    modal: {
+      enabled: false,
+      // header: 'Possible closures',
+      // body: '<p>All City-run COVID-19 testing sites and health centers are open and on a normal schedule, though others may be closed. Please call ahead to ensure that the site you are going to is open.</p>',
+      header: 'Before you go',
+      body: '<p>Some test sites may be closed due to winter weather conditions.  Always call ahead before you go to a site.</p>',
+    },
+    // header: {
+    //   type: 'alertBanner',
+    //   // enabled: function(state) {
+    //   //   return state.alertResponse === 'alertHours';
+    //   // },
+    //   // content: '<b>Until further notice:</b> Please call ahead or check hours of operation while business restrictions are still in effect.',
+    // },
+    alertChecks: [
+      // {
+      //   type: 'alertHours',
+      //   condition: [
+      //     {
+      //       'day': 1,
+      //       'startTime': '1:00',
+      //       'endTime': '23:59',
+      //     },
+      //     {
+      //       'day': 2,
+      //       'startTime': '1:00',
+      //       'endTime': '23:59',
+      //     },
+      //   ],
+      // },
+    ],
+  },
+  app: {
+    title: 'Fun Hikes',
+    logoAlt: 'logo',
+    type: 'hikes',
+    trustedSite: 'hidden',
+    skipGreeting: true,
+  },
+  // resetDataOnGeocode: true,
+  retractableRefine: false,
+  dropdownRefine: false,
+  searchBar: {
+    placeholder: 'Search by address',
+    searchTypes: [ 'address' ],
+    labelText:  {
+      address: 'Search by address',
+    },
+    placeholderText: {
+      address: 'Search by address',
+    },
+  },
+  locationInfo: {
+    siteName: function(item) {
+      return item.fields.Name;
+    },
+  },
+  customComps,
+  // baseConfig: BASE_CONFIG_URL,
+  // holidays: {
+  //   days: ['Monday'],
+  // },
+  // hiddenRefine: {
+  //   City: function(item) {
+  //     return item.attributes.City === 'Philadelphia';
+  //   },
+  //   Visibility: function(item) {
+  //     return item.attributes.Visibility === 'pub' || item.attributes.Visibility === 'For Public View';
+  //   },
+  // },
+  refine: {
+    type: 'categoryField_array',
+    columns: false,
+    value: function(item) {
+      return item.fields.type;
+    },
+  },
+  markerType: 'circle-marker',
+  circleMarkers:{
+    color: '#FF0000',
+    weight: 0,
+    radius: 8,
+    mobileRadius: 12,
+    size: 10,
+    mobileSize: 20,
+  },
+  cyclomedia: {
+    enabled: false,
+    measurementAllowed: false,
+    popoutAble: true,
+    recordingsUrl: 'https://atlas.cyclomedia.com/Recordings/wfs',
+    username: process.env.VUE_APP_CYCLOMEDIA_USERNAME,
+    password: process.env.VUE_APP_CYCLOMEDIA_PASSWORD,
+    apiKey: process.env.VUE_APP_CYCLOMEDIA_API_KEY,
+  },
+  dataSources: {
+    hikes,
+  },
+  router: {
+    enabled: false,
+  },
+  // projection: '3857',
+  geocoder: {
+    url(input) {
+      const inputEncoded = encodeURIComponent(input);
+      return `//api.phila.gov/finder/v1/search/${inputEncoded}`;
+    },
+    params: {
+      include_units: true,
+    },
+  },
+  footer: [
+    // {
+    //   type: "native",
+    //   href: "https://www.phila.gov/",
+    //   attrs: {
+    //     target: "_blank",
+    //   },
+    //   text: "cityOfPhiladelphia",
+    // },
+    // {
+    //   type: "native",
+    //   href: "#",
+    //   text: "about",
+    // },
+    // {
+    //   type: "native",
+    //   href: "https://www.phila.gov/feedback/",
+    //   attrs: {
+    //     target: "_blank",
+    //   },
+    //   text: "feedback",
+    // },
+  ],
+  map: {
+    // type: 'leaflet',
+    type: 'mapbox',
+    tiles: 'hosted',
+    containerClass: 'map-container',
+    defaultBasemap: 'pwd',
+    center: [ -75.163471, 39.953338 ],
+    minZoom: 11,
+    maxZoom: 25,
+    shouldInitialize: true,
+
+    zoom: 9,
+    geocodeZoom: 15,
+    imagery: {
+      enabled: false,
+    },
+    basemaps: {
+      pwd: {
+        url: 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer',
+        tiledLayers: [
+          'cityBasemapLabels',
+        ],
+        type: 'featuremap',
+        attribution: 'Parcels: Philadelphia Water',
+      },
+    },
+    tiledLayers: {
+      cityBasemapLabels: {
+        url: 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer',
+        zIndex: '3',
+      },
+    },
+  },
+  mbStyle: 'mapbox://styles/mapbox/satellite-streets-v12?access_token=pk.eyJ1IjoiYWpyb3Rod2VsbCIsImEiOiJXa3ZKNDg0In0.gFdKTdvy550fY_iy16YkSA',
+  // mbStyle: 'mapbox://styles/ajrothwell/ck6gz6rmk04681ir1fdmagq5h',
+  // mbStyle: 'https://api.mapbox.com/v4/mapbox.satellite/1/0/0@2x.jpg90?access_token=pk.eyJ1IjoiYWpyb3Rod2VsbCIsImEiOiJXa3ZKNDg0In0.gFdKTdvy550fY_iy16YkSA',
+  // mbStyle: 'mapbox://styles/ajrothwell/ck6gz6rmk04681ir1fdmagq5h',
+  // mbStyle: {
+  //   version: 8,
+  //   sources: {
+  //     pwd: {
+  //       tiles: [
+  //         'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer/tile/{z}/{y}/{x}',
+  //       ],
+  //       type: 'raster',
+  //       tileSize: 256,
+  //     },
+  //   },
+  //   layers: [
+  //     {
+  //       id: 'pwd',
+  //       type: 'raster',
+  //       source: 'pwd',
+  //     },
+  //   ],
+  // },
+  basemapSources: {
+    pwd: {
+      source: {
+        tiles: [
+          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer/tile/{z}/{y}/{x}',
+          // '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}'
+        ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'pwd',
+        type: 'raster',
+      },
+    },
+    imagery2019: {
+      source: {
+        tiles: [
+          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2019_3in/MapServer/tile/{z}/{y}/{x}',
+          // '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}'
+        ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'imagery2019',
+        type: 'raster',
+      },
+    },
+  },
+  basemapLabelSources:{
+    cityBasemapLabels: {
+      source: {
+        tiles: [ 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}' ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'cityBasemapLabels',
+        type: 'raster',
+      },
+    },
+    imageryBasemapLabels: {
+      source: {
+        tiles: [ 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_Labels/MapServer/tile/{z}/{y}/{x}' ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'imageryBasemapLabels',
+        type: 'raster',
+      },
+    },
+  },
+});
